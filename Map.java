681180d72feba5dfd784631eae79f56cf974a7a4
nordqunn <nordqunn@rose-hulman.edu>
2019-02-13 21:34:36 -0500

    cleared out the junk

--- Map.java
+++
@@ -1,7 +0,0 @@
-public class Map{
-	static void main(String[] args){
-		System.out.println("First thing: Hello from Nate!");
-		System.out.println("Second thinnng: mistkae from Ntae"); //TODO: fix
-	}
-}
-
